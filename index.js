3c173aebfbb4ef40c455c35d5c3c663914c2b5c3
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-05-19 18:16:06 +0300

    add user model and controller

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// import * as UsersController from './controllers/UserController.js';
+import * as UsersController from './controllers/UserController.js';
 // import * as BlogController from './controllers/BlogController.js';
 // import * as CalculatorController from './controllers/CalculatorController.js';
 
@@ -19,11 +19,11 @@ mongoose
 app.use(cors());
 app.use(express.json())
 
-// app.post('/register-user',UsersController.register);
-// app.post('/login-user',UsersController.login);
-// app.delete('/remove-user',UsersController.removeUser);
-// app.patch('/update-balance',UsersController.updateBalance);
-// app.patch('/update-discount',UsersController.updateDiscount);
+app.post('/register-user',UsersController.register);
+app.post('/login-user',UsersController.login);
+app.delete('/remove-user',UsersController.removeUser);
+app.patch('/update-balance',UsersController.updateBalance);
+app.patch('/update-discount',UsersController.updateDiscount);
 // app.post('/create-post',BlogController.addNewPost);
 // app.patch('/update-post',BlogController.updatePost);
 // app.delete('/remove-post',BlogController.removePost);
--- models/User.js
+++ models/User.js
@@ -1,20 +1,20 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const UserSchema = new mongoose.Schema({
-//     email: {
-//         required: true,
-//         type: String,
-//         unique: true,
-//     },
-//     name: String,
-//     password: {
-//         required: true,
-//         type: String,
-//     },
-//     isAdmin: Boolean,
-//     balance: Number,
-//     discount: Boolean,
-//     discountValue: Number
-// },{timestamps: true,})
+const UserSchema = new mongoose.Schema({
+    email: {
+        required: true,
+        type: String,
+        unique: true,
+    },
+    name: String,
+    password: {
+        required: true,
+        type: String,
+    },
+    isAdmin: Boolean,
+    balance: Number,
+    discount: Boolean,
+    discountValue: Number
+},{timestamps: true,})
 
-// export default mongoose.model('User',UserSchema)
+export default mongoose.model('User',UserSchema)
